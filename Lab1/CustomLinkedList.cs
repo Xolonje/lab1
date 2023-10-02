5abc8cc1fc61fc74871bb577708f48751d836cde
Asya Epshtein <xolonje@proton.me>
2023-10-02 15:01:45 +0300

    Implementation of the Custom Linked List class

---
+++ Lab1/CustomLinkedList.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+
+namespace Lab1;
+
+public class CustomLinkedList<T> : IEnumerable<T>
+{
+    public IEnumerator<T> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+}
